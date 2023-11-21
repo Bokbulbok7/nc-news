9e513562d8d0ae8df759be4ca7e4266d5eb11d0c
bokbul <pooja.meera@yahoo.com>
2023-11-21 12:14:30 +0000

    task 5

--- db/controllers/articlesController.js
+++ db/controllers/articlesController.js
@@ -1,10 +1,22 @@
-const { selectArticleById } = require("../models/articlesModel");
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articlesModel");
 
 exports.getArticleById = (req, res, next) => {
   const { articleId } = req.params;
   selectArticleById(articleId)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getArticles = (req, res, next) => {
+  const { sort_by } = req.query;
+  selectArticles(sort_by)
+    .then((articles) => {
+      res.status(200).send({ articles: articles });
     })
     .catch(next);
 };
--- db/models/articlesModel.js
+++ db/models/articlesModel.js
@@ -10,3 +10,15 @@ exports.selectArticleById = (articleId) => {
       return result.rows[0];
     });
 };
+
+exports.selectArticles = (sort_by = "created_at") => {
+  let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, 
+  articles.created_at, articles.votes, articles.article_img_url, COUNT (comments.comment_id):: int AS comment_count
+  FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id 
+  GROUP BY  articles.author, articles.title, articles.article_id, articles.topic, 
+  articles.created_at, articles.votes, articles.article_img_url
+  ORDER BY articles.${sort_by} DESC;`;
+  return db.query(queryString).then((data) => {
+    return data.rows;
+  });
+};
