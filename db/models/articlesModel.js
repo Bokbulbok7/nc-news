bc1ac09f2a3db0d03da28d69349237e1991b6048
bokbul <pooja.meera@yahoo.com>
2023-11-23 15:50:51 +0000

    task 8

--- db/models/articlesModel.js
+++ db/models/articlesModel.js
@@ -33,3 +33,14 @@ exports.checkArticleExists = (articleId) => {
       return result.rows;
     });
 };
+
+exports.updateVotesByArticleId = (articleId, incVotes) => {
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`,
+      [incVotes, articleId]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
