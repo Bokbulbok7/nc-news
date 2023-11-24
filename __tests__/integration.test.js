0d1c5676e4f6793a88498c2a9904a12fd5090e46
Meera Gopi <pooja.meera@yahoo.com>
2023-11-24 11:07:43 +0000

    Update integration.test.js
    inc_votes

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -266,7 +266,7 @@ describe("POST /api/articles/:article_id/comments", () => {
 
 describe("PATCH /api/articles/:article_id", () => {
   it("PATCH: 200 returns the updated article with the incremented votes by the given amount.", () => {
-    const votes = { incVotes: 5 };
+    const votes = { inc_votes: 5 };
     const expectedArticle = {
       article_id: 1,
       title: "Living in the shadow of a great man",
@@ -289,7 +289,7 @@ describe("PATCH /api/articles/:article_id", () => {
   });
   
   it("PATCH: 200 returns the updated article with the decremented votes by the given amount.", () => {
-    const votes = { incVotes: -7 };
+    const votes = { inc_votes: -7 };
     const expectedArticle = {
       article_id: 1,
       title: "Living in the shadow of a great man",
