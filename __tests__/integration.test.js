1ae451802df5bfb86c11ad5a7dcf2a453b4b64fe
bokbul <pooja.meera@yahoo.com>
2023-11-24 11:03:27 +0000

    task 8

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -266,7 +266,7 @@ describe("POST /api/articles/:article_id/comments", () => {
 
 describe("PATCH /api/articles/:article_id", () => {
   it("PATCH: 200 returns the updated article with the incremented votes by the given amount.", () => {
-    const votes = { incVotes: 5 };
+    const votes = { inc_votes: 5 };
     const expectedArticle = {
       article_id: 1,
       title: "Living in the shadow of a great man",
@@ -287,9 +287,9 @@ describe("PATCH /api/articles/:article_id", () => {
         expect(body.article).toEqual(expectedArticle);
       });
   });
-  
+
   it("PATCH: 200 returns the updated article with the decremented votes by the given amount.", () => {
-    const votes = { incVotes: -7 };
+    const votes = { inc_votes: -7 };
     const expectedArticle = {
       article_id: 1,
       title: "Living in the shadow of a great man",
